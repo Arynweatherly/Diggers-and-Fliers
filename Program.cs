04c5db2beb9c0d281a73e810fd716edc2ea4aeac
aryn weatherly <arynweatherly@aryns-MacBook-Pro.local>
2019-11-28 23:48:07 -0600

    added some animals

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DiggersAndFliers
 {
@@ -11,6 +13,18 @@ namespace DiggersAndFliers
             List<Interfaces.IFlying> birdCage = new List<Interfaces.IFlying>();
             List<Interfaces.ISwimmers> pond = new List<Interfaces.ISwimmers>();
             List<Interfaces.IAnimals> zoo = new List<Interfaces.IAnimals>();
+
+            var myAnts = new Animals.Ants();
+            var myBettaFish = new Animals.BettaFish();
+            var myCopperheadSnakes = new Animals.CopperheadSnakes();
+            var myEarthworms = new Animals.Earthworms();
+            var myFinches = new Animals.Finches();
+            var myGirbils = new Animals.Gerbils();
+            var myMice = new Animals.Mice();
+            var myParakeets = new Animals.Parakeets();
+            var myTerrapins = new Animals.Terrapins();
+            var myTimberRattlesnakes = new Animals.TimberRattlesnakes();
+
 
         }
     }
